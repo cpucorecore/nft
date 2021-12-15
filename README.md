ac295b3b3d4971f6f87bc699b44cf79cf3b7b9ae
刘坤瑜 <sky@liukunyudeMacBook-Pro.local>
2021-12-15 16:01:44 +0800

    fisco bcos deployment

--- README.md
+++ README.md
@@ -1,43 +1,5 @@
 https://app.yinxiang.com/fx/1b0c1d05-4987-4e88-9191-a76714977cbd
 
-# 环境准备
-- install npm
-- install truffle
-
-# 安装
-```shell
-npm install .
-```
-
-# 运行demo
-- 进入truffle develop开发环境
-```shell
-truffle develop
-```
-
-- 在truffle开发环境中部署合约
-```shell
-truffle(develop)> migrate
-```
-
-- 与部署的nft合约交互
-```shell
-# 创建nft合约对象wines
-truffle(develop)> wines = await Wines.deployed()
-undefined
-
-# 发行nft，指定发行的目标地址和该nft的uri链接
-truffle(develop)> wines.issueWineNFT('0x5803f9025b1de9c76484fbd2b5976875bd669d5e', 'https://www.xxwine.com/products/tinghua/1.json')
-
-# 查看nft的url
-truffle(develop)> await wines.tokenURI(1)
-
-# 查看nft的所有者
-truffle(develop)> wines.ownerOf(1)
-
-# 销毁nft
-truffle(develop)> wines.burnWineNFT(1)
-```
-
-
-# truffle develop开发环境每次重新进入会重置数据
+- 为了部署到fisco bcos，需要按照其要求准备编译合约的环境，目录结构等
+- 基于openzepplin 4.4.0版本构建，为了适应fisco bcos的合约编译环境只是将目录结构调整成同一目录，各合约引用的路径修改了，合约内容不做任何修改
+- openzepplin 4.4.0合约中指定的solidity版本是0.8.0，但是fisco bcos只支持0.4/0.5/0.6，所以再将所有openzepplin的合约中的sol版本号改成0.6.0
--- contracts/Wines.sol
+++ contracts/Wines.sol
@@ -1,26 +1,18 @@
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "./ERC721URIStorage.sol";
 
-contract Wines is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-
+contract Wines is ERC721URIStorage {
     constructor(string memory name, string memory symbol, string memory baseURI) ERC721(name, symbol) {}
 
-    function issueWineNFT(address to, string memory tokenURI) public onlyOwner returns (uint256)
+    function issueWineNFT(address to, uint256 tokenId, string memory tokenURI) public
     {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _mint(to, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
     }
 
-    function burnWineNFT(uint256 tokenId) public onlyOwner {
+    function burnWineNFT(uint256 tokenId) public
+    {
         _burn(tokenId);
     }
 }
