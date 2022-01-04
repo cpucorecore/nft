d8d31adfd127b993ff3c6be4d4b1d571c2c1540e
cpucorecore <cpucorecore@gmail.com>
2022-01-04 18:01:21 +0800

    update

--- contracts/Evidence.sol
+++ contracts/Evidence.sol
@@ -2,18 +2,18 @@ pragma solidity >=0.6.0 <0.8.0;
 pragma experimental ABIEncoderV2;
 
 contract Evidence {
-    mapping(uint256 => bytes32[]) private tokenId2txIds;
+    mapping(uint256 => string[]) private tokenId2txIds;
     mapping(uint256 => string[]) private tokenId2exts;
 
-    function saveReceipt(uint256 tokenId, bytes32 txId, string memory ext) public {
-        bytes32[] storage txIds = tokenId2txIds[tokenId];
+    function saveReceipt(uint256 tokenId, string memory txId, string memory ext) public {
+        string[] storage txIds = tokenId2txIds[tokenId];
         txIds.push(txId);
 
         string[] storage exts = tokenId2exts[tokenId];
         exts.push(ext);
     }
 
-    function getTxIds(uint256 tokenId) public view returns (bytes32[] memory) {
+    function getTxIds(uint256 tokenId) public view returns (string[] memory) {
         return tokenId2txIds[tokenId];
     }
 
