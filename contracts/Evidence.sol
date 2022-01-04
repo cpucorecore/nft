8c81cc1448f331dc635848cd887c1acb89f507c2
cpucorecore <cpucorecore@gmail.com>
2022-01-04 11:07:13 +0800

    update Evidence.sol

--- contracts/Evidence.sol
+++ contracts/Evidence.sol
@@ -2,14 +2,20 @@ pragma solidity >=0.6.0 <0.8.0;
 pragma experimental ABIEncoderV2;
 
 contract Evidence {
-    mapping(uint256 => bytes32[]) private tokenId2txIds;
-
-    function saveTxId(uint256 tokenId, bytes32 txId) public {
-        bytes32[] storage txIds = tokenId2txIds[tokenId];
-        txIds.push(txId);
+    struct Receipt {
+        bytes32 txId;
+        string ext;
     }
 
-    function getTxIds(uint256 tokenId) public view returns (bytes32[] memory txIds) {
-        return tokenId2txIds[tokenId];
+    mapping(uint256 => Receipt[]) private tokenId2receipts;
+
+    function saveReceipt(uint256 tokenId, bytes32 txId, string memory ext) public {
+        Receipt[] storage receipts = tokenId2receipts[tokenId];
+        Receipt memory receipt = Receipt(txId, ext);
+        receipts.push(receipt);
+    }
+
+    function getReceipts(uint256 tokenId) public view returns (Receipt[] memory receipts) {
+        return tokenId2receipts[tokenId];
     }
 }
--- test/EvidenceTests.js
+++ test/EvidenceTests.js
@@ -6,37 +6,51 @@ contract("Evidence", accounts => {
     it("after mint 1 nft then burn 1 nft", () => {
         let evidence;
         let txId1 = "0xfd73d78160e163067451f8ae48109d64228d9d363fca25d004c95ed40689f725";
-        let txId2 = "0x4ad1e53ed315fbf1b9c4c16f0b5e89e6da14ae193fb00d95c266f5554fc4b7d3"
+        let ext1 = "{\"k1\":\"v1\"}";
+        let txId2 = "0x4ad1e53ed315fbf1b9c4c16f0b5e89e6da14ae193fb00d95c266f5554fc4b7d3";
+        let ext2 = "{\"k1\":\"v1\"}";
 
         return Evidence.deployed()
             .then(instance => {
                 evidence = instance;
-                return evidence.saveTxId(1, txId1);
+                return evidence.saveReceipt(1, txId1, ext1);
             })
             .then(() => {
-                return evidence.saveTxId(1, txId2);
+                return evidence.saveReceipt(1, txId2, ext2);
             })
-            .then(() => evidence.getTxIds.call(1))
+            .then(() => evidence.getReceipts.call(1))
             .then(value => {
-                let txIds = value.valueOf();
+                let receipts = value.valueOf();
 
                 assert.equal(
-                    txIds.length,
+                    receipts.length,
                     2,
                     "total supply not be 1"
                 );
 
                 assert.equal(
-                    txIds[0],
+                    receipts[0].txId,
                     txId1,
-                    ""
+                    "txId should be equal"
                 );
 
                 assert.equal(
-                    txIds[1],
+                    receipts[0].ext,
+                    ext1,
+                    "ext should be equal"
+                )
+
+                assert.equal(
+                    receipts[1].txId,
                     txId2,
-                    ""
+                    "txId should be equal"
                 );
+
+                assert.equal(
+                    receipts[1].ext,
+                    ext2,
+                    "ext should be equal"
+                )
             })
     })
 })
